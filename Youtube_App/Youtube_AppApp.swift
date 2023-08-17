491a91e8dc96e489e3425a3785fc7598f8a9bcb1
Naina <ns1388@scarletmail.rutgers.edu>
2023-08-16 20:14:58 -0400

    Initial Commit

---
+++ Youtube_App/Youtube_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Youtube_AppApp.swift
+//  Youtube_App
+//
+//  Created by Nayana Sharma on 8/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Youtube_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
